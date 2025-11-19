1df3e625d8fbd4aa8043c89c88c7e51789423ad4
Leandro-Kolosque <161510235+Leandro-Kolosque@users.noreply.github.com>
2025-11-19 16:05:54 -0300

    Fim dos exercícios de laço

---
+++ lacos-repeticao/calculando_soma.py
@@ -0,0 +1,12 @@
+# REVISÃO DEDICADA A LAÇOS DE REPETIÇÃO
+
+# Calculando a soma de números
+# Você está recebendo uma lista de valores representando os produtos de sua loja virtual e gostaria de calcular a soma total desses produtos para entender o desempenho financeiro semanal.
+
+valores = [10, 20, 30, 40, 50]
+soma = 0
+
+for valor in valores:
+    soma += valor
+
+print(soma)
